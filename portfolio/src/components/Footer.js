7c4e17a0cf0319e543bec8d13056c9973ba8ba60
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-19 14:18:54 -0400

    fixed something

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
-import styles from './Footer.css';
+import './Footer.css';
 import { IconContext } from 'react-icons';
 
 const Footer = () => {
