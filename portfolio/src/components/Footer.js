67af61eb5fbb9be476306064fa37c31ac0fa2d1d
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-19 12:13:03 -0400

    figured out a way of how to style react icons

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,17 +1,26 @@
-import React from 'react'; 
-import { FaLinkedinIn } from "react-icons/fa";
+import React from 'react';
+import { FaLinkedinIn } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa'; 
-
+import { FaTwitter } from 'react-icons/fa';
+import styles from './Footer.css';
+import { IconContext } from 'react-icons';
 
 const Footer = () => {
   return (
-    <div>
-      <FaLinkedinIn />
-      <FaGithub />
-      <FaTwitter /> 
-    </div>
+    <>
+      <IconContext.Provider value={{ className: 'main-icons' }}>
+        <IconContext.Provider value={{ className: 'linkedin-icon' }}>
+          <FaLinkedinIn />
+        </IconContext.Provider>
+        <div>
+          <FaGithub />
+        </div>
+        <div>
+          <FaTwitter />
+        </div>
+      </IconContext.Provider>
+    </>
   );
-}
+};
 
-export default Footer
+export default Footer;
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react'
 import About from '../display/About'; 
 import Portfolio from '../display/Portfolio'; 
 import Contact from '../display/Contact'; 
+import styles from './Header.css'
 
 const Header = () => {
   return (
     <>
-      <About />
-      <Portfolio />
-      <Contact />
+      <div className={styles['main-header']}>
+        <About />
+        <Portfolio />
+        <Contact />
+      </div>
     </>
-  )
+  );
 }
 
 export default Header
