d1083b87cb0c9fac12c1a5d32ce6674de088d9ea
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-19 22:20:47 -0400

    added link to the header

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import About from '../display/About'; 
-import Portfolio from '../display/Portfolio'; 
+import Portfolios from '../display/Portfolios'; 
 import Contact from '../display/Contact'; 
 import styles from './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
-      <div className={styles['main-header']}>
-        <About />
-        <Portfolio />
-        <Contact />
+      <div className={'main-header'}>
+        <Link to="/about">
+          <About />
+        </Link>
+        <Link to="/portfolios">
+          <Portfolios />
+        </Link>
+        <Link to="/contact">
+          <Contact />
+        </Link>
       </div>
     </>
   );
--- portfolio/src/display/About.js
+++ portfolio/src/display/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <>
       About
-    </div>
+    </>
   )
 }
 
