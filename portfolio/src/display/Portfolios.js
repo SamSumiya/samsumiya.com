9fe645886ff58792e04def4b7dece1aaa4091de7
chenerychen <60025491+chenerychen@users.noreply.github.com>
2021-10-20 23:51:23 -0400

    styling the portfolios now

--- portfolio/src/display/Portfolios.js
+++ portfolio/src/display/Portfolios.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import './Portfolios.css'
 
 const Portfolios = () => {
   return (
-    <div>
-      dsa
-    </div>
-  )
+    <>
+      Web Developer Portfolio
+      <ul className="projects-grid-container">
+        <li className="grid-item">TALES OF TORR 2</li>
+        <li className="grid-item">Brewster API</li>
+        <li className="grid-item">Todos APP</li>
+      </ul>
+    </>
+  );
 }
 
 export default Portfolios
